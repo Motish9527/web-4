f1119a75a877795455c5aa9427455628c75260c8
Motish9527 <yymotish@gmail.com>
2025-03-13 15:53:09 +0800

    recording sec
    剩最後十五分鐘，沒在聽了、、、
    記得補影片檔

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-red-500">
+      <h1>Motish</h1>
+      <h1>Motish</h1>
+      <h1>Motish</h1>
+      <h1>Motish</h1>
+      <h1>Motish</h1>
+      <h1>Motish</h1>
+      <h1>Motish</h1>
+    </div>
+  );
+}
